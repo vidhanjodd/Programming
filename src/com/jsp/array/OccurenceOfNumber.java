d070c65cf1f23646c6d974a09f7382834526ebae
vidhanjodd <vidhanlonare@gmail.com>
2025-08-01 21:24:04 +0530

    new programns

--- src/com/jsp/array/OccurenceOfNumber.java
+++ src/com/jsp/array/OccurenceOfNumber.java
@@ -2,8 +2,8 @@ package com.jsp.array;
 
 public class OccurenceOfNumber {
     public static void main(String[] args) {
-        int arr[] = {10,20,30,10,20};
-        boolean counted[] = new boolean[arr.length];
+        int[] arr = {10,20,30,10,20};
+        boolean[] counted = new boolean[arr.length];
         for(int i=0;i<arr.length;i++){
             if (!counted[i]){
                 int count = 0;
